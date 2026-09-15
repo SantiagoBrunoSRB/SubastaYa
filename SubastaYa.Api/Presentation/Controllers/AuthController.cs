ca19aaa14850296e1fdbd42b79c974939d70b087
SantiagoBrunoSRB <santiagorafaelbrunosrb@gmail.com>
2026-09-15 02:19:38 -0300

    feat(presentation): agregar AuthController

---
+++ SubastaYa.Api/Presentation/Controllers/AuthController.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Mvc;
+using SubastaYa.Api.Application.DTOs.Auth;
+using SubastaYa.Api.Application.Interfaces.Services;
+
+namespace SubastaYa.Api.Presentation.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AuthController : ControllerBase
+{
+    private readonly IAuthService _authService;
+
+    public AuthController(IAuthService authService)
+    {
+        _authService = authService;
+    }
+
+    [HttpPost("login")]
+    public async Task<IActionResult> Login([FromBody] LoginRequestDto request)
+    {
+        var response = await _authService.LoginAsync(request);
+        return Ok(response);
+    }
+
+    [HttpPost("register")]
+    public async Task<IActionResult> Register([FromBody] RegisterRequestDto request)
+    {
+        var response = await _authService.RegisterAsync(request);
+        return Ok(response);
+    }
+}
